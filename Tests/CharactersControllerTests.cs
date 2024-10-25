48ec41948a8e3e857f429589f7950f6aec1ba56e
Zobair Qauomi <Zobairq55@gmail.com>
2024-10-25 20:39:21 +0200

    correct my deliberate mistake by not making changes without proper inspection

--- Tests/CharactersControllerTests.cs
+++ Tests/CharactersControllerTests.cs
@@ -13,7 +13,7 @@ namespace RickAndMortyRestApi.Tests
         public CharactersControllerTests()
         {
             _mockGraphQLClient = new Mock<IRickAndMortyGraphQLClient>();
-            _controller = new CharactersController(_mockGraphQLClient.Object,null);
+            _controller = new CharactersController(_mockGraphQLClient.Object, new Mock<ILogger<CharactersController>>().Object);
         }
 
         [Fact]
